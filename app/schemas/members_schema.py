5184835513194f326ec41d6a95c4a1458690a03d
CristianArias <cristianarias813@gmail.com>
2025-09-20 08:30:46 -0300

    Falla v1 cruces members y parajes

--- app/schemas/members_schema.py
+++ app/schemas/members_schema.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from datetime import date
 from typing import Optional
+from app.schemas.parajes_schema import Paraje
 
 class Member(BaseModel):
     dni: int
@@ -10,9 +11,14 @@ class Member(BaseModel):
     phone: str
     email: str
     birth_date: date 
-    paraje_id: int
+    #paraje_id: int
     join_date: Optional[date] = None
+    paraje: Paraje  # Relación con Paraje
     # join_date es opcional porque se asigna automaticamente con default=func.current_date al crear el miembro
+    class Config:
+        orm_mode = True
+
+    
 
 
 
--- app/schemas/parajes_schema.py
+++ app/schemas/parajes_schema.py
@@ -3,3 +3,5 @@ from pydantic import BaseModel
 class Paraje(BaseModel):
     name: str
     
+    class Config:
+        orm_mode = True
