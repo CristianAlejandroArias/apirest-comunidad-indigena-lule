f7e2c24809a6747aea2ea2f9605c442cdca8b195
CristianArias <cristianarias813@gmail.com>
2025-09-20 00:41:58 -0300

    Cheque existencia de paraje para member.

--- app/services/members_services.py
+++ app/services/members_services.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from fastapi import HTTPException
 from app.repositories import MembersRepository
 from app.repositories.models.members_model import MemberModel
+from app.repositories.parajes_repository import ParajesRepository
 
 class MembersService:
     def __init__(self):
@@ -20,8 +21,16 @@ class MembersService:
                 status_code=400,
                 detail="El email ya está registrado."
             )
-        else: 
-            return self.repository.create_member(db, member)
+        else:
+            #Ahora voy a ver que el paraje_id exista
+            existing_paraje = ParajesRepository().get_paraje(db, member.paraje_id)
+            if not existing_paraje:
+                raise HTTPException(
+                    status_code=400,
+                    detail="El paraje_id no existe."
+                ) 
+            else:
+                return self.repository.create_member(db, member)
     
     def update_member(self, db: Session, member_id: int, member: MemberModel):
         return self.repository.update_member(db,member_id,member)
